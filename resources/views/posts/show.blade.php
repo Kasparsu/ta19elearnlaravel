cc98c2b498ea2170a60661a8490b33e62380729e
RaitBoach <RaitBoach@gmail.com>
2021-11-08 09:42:48 +0200

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,14 +3,15 @@
 @section('content')
     <a class="btn btn-primary" href="{{url()->previous()}}">Back</a>
     @foreach($posts as $post)
-    <div class="col">
-        <div class="card mt-3">
-            <div class="card-body">
-                <h5 class="card-title">{{$post->title}}</h5>
-                <p class="card-text">{{$post->body}}</p>
-                <a href="card-text">{{$post->created_at}}"</a>
+        <div class="col">
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h5 class="card-title">ID: {{$post->id}}</h5>
+                    <h5 class="card-title">Title: {{$post->title}}</h5>
+                    <p class="card-text">Description: {{$post->body}}</p>
+                    <a href="card-text">Created at: {{$post->created_at}}"</a>
+                </div>
             </div>
         </div>
-    </div>
     @endforeach
 @endsection
